facfc7ef13d04de350ae2dea376f67c62b886ccd
mathzxw2002 <mathzxw2002@gmail.com>
2025-08-20 11:19:21 +0800

    Create listener.py

---
+++ nav/ubrobot_ros2/src/ubrobot/ubrobot/listener.py
@@ -0,0 +1,63 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+import logging
+import os
+import time
+from dataclasses import asdict
+from pprint import pformat
+
+# from safetensors.torch import load_file, save_file
+'''from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.common.policies.factory import make_policy
+from lerobot.common.robot_devices.control_configs import (
+    CalibrateControlConfig,
+    ControlConfig,
+    ControlPipelineConfig,
+    RecordControlConfig,
+    RemoteRobotConfig,
+    ReplayControlConfig,
+    TeleoperateControlConfig,
+)
+from lerobot.common.robot_devices.control_utils import (
+    control_loop,
+    init_keyboard_listener,
+    is_headless,
+    log_control_info,
+    record_episode,
+    reset_environment,
+    sanity_check_dataset_name,
+    sanity_check_dataset_robot_compatibility,
+    stop_recording,
+    warmup_record,
+)
+from lerobot.common.robot_devices.robots.utils import Robot, make_robot_from_config
+from lerobot.common.robot_devices.utils import busy_wait, safe_disconnect
+from lerobot.common.utils.utils import has_method, init_logging, log_say
+from lerobot.configs import parser
+'''
+
+class Listener(Node):
+    def __init__(self):
+        super().__init__('listener_node')
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.listener_callback,
+            10)
+        self.subscription  # 防止未使用变量警告
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'Received: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Listener()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
